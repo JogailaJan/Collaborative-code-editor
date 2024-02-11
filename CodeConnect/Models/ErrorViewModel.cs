1d98e67b2dd61d949ad8b209994b525ee3205351
JogailaJan <jogaila.janusas@gmail.com>
2024-02-11 20:14:10 +0200

    Started working on authentication
    Created a ASP.NET Core MVC Project and working on login/register. Currently on the 22nd inute of the tutorial. Just managed to connect the DB

---
+++ CodeConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
